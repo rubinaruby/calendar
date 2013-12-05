44ce8f306568d83548225dcb8a8336f49999bb75
rubinaruby <rubina@probytes.in>
2013-12-05 19:58:16 +0530

    assets compiled for heroku rubina

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,9 @@
 class Appointment < ActiveRecord::Base
 	belongs_to :worker
+	before_create :event_duration
 	
+	private
+
 	def event_duration
 		self.duration_time = self.end_time - self.start_time
 	end	
